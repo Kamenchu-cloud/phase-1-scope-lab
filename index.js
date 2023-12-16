c240438253c26669a8e199348393aaf8f6aeb3ce
Kamenchu <iankamenchu@gmail.com>
2023-12-16 22:03:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newValue) {
+  window.bestCustomer = newValue;
+}
+
+const leastFavoriteCustomer = 'not-Ian';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new not-Ian'; 
+}
